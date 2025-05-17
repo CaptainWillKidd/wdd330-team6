5218276148f885b1fd8d9a3f5b3bd96c01295f29
CaptainWillKidd <can24018@byui.edu>
2025-05-17 09:48:13 -0300

    i added the function to add items and to remove it

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,44 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+  const productList = document.querySelector(".product-list");
+  if (!cartItems.length) {
+    productList.innerHTML = "<li>Your Shopping Cart is Empty.</li>";
+    return;
+  }
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  productList.innerHTML = htmlItems.join("");
+
+  document.querySelectorAll(".remove-item").forEach(btn => {
+    btn.addEventListener("click", removeFromCart);
+  });
 }
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+function cartItemTemplate(item, index) {
+  return `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img src="${item.Image}" alt="${item.Name}" />
+      </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+      <p class="cart-card__color">${item.Colors && item.Colors[0] ? item.Colors[0].ColorName : ''}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+      <span class="remove-item" data-index="${index}" style="cursor:pointer;color:red;float:right;font-weight:bold;">X</span>
+    </li>
+  `;
+}
 
-  return newItem;
+function removeFromCart(event) {
+  const index = event.target.getAttribute("data-index");
+  let cartItems = getLocalStorage("so-cart") || [];
+  cartItems.splice(index, 1);
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
 }
 
 renderCartContents();
