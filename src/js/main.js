e1a2dfc8dee6014a046a8532bd758c1e6f0cc3f8
JJFRENZY <jackjayeck@icloud.com>
2025-05-19 10:44:41 -0600

    errors fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 import ProductData from './ProductData.mjs';
-const dataSource = new ProductData('tents');
 import ProductList from './ProductList.mjs';
 
+const dataSource = new ProductData('tents');
 const productListElement = document.querySelector('.product-list'); // or your specific selector
 const tentList = new ProductList('tents', dataSource, productListElement);
 tentList.init();
+
